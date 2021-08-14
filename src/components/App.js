3d30b19af29bd590012c68f36c4f076255606e00
karim <k.guezlane@gmail.com>
2021-08-14 21:23:14 +0100

    adding home and signeup routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import "../style/App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./Home";
+import SignUp from "./Home-components/SignUp";
+import { AuthProvider } from "../context/AuthContext";
 
 function App() {
 	return (
-		<div className='App'>
+		<BrowserRouter className='App'>
 			<header className='App-header'></header>
-		</div>
+			<AuthProvider>
+				<Switch>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/signup' component={SignUp} />
+				</Switch>
+			</AuthProvider>
+		</BrowserRouter>
 	);
 }
 
