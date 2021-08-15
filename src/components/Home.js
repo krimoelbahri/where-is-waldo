8142e45240d432cebb357a90b92aebf748e166f6
karim <k.guezlane@gmail.com>
2021-08-15 22:38:03 +0100

    styling home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import SignIn from "./Home-components/SignIn";
+import LogIn from "./LogIn";
+import styled from "styled-components";
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 100px auto;
+	border: 1px solid grey;
+	min-width: 300px;
+	height: 350px;
+	justify-content: center;
+`;
+const Title = styled.h2`
+	display: flex;
+	align-items: center;
+	height: 30px;
+	text-align: center;
+	font-weight: bold;
+	font-size: large;
+	flex-direction: row;
+`;
 
 export default function Home() {
 	return (
-		<div id='Home' className='container'>
-			<h1>Where's Waldo </h1>
-			<div>
-				<div>
-					<h2>Sign in to play</h2>
-					<SignIn />
-					<h2>
-						{" "}
-						You don't have an account? <Link to='/signup'>
-							Sign Up
-						</Link>{" "}
-					</h2>
-					<button id='guestButton'>
-						<p>Play as Guest</p>
-					</button>
-				</div>
-			</div>
-		</div>
+		<Container>
+			<Title>Sign in to play</Title>
+			<LogIn />
+		</Container>
 	);
 }
