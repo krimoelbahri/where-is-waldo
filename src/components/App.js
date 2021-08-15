a115355a35b0bbb5c25bc223e14da08050c26d3f
karim <k.guezlane@gmail.com>
2021-08-15 22:39:51 +0100

    styling header

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import "../style/App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
 import Home from "./Home";
-import SignUp from "./Home-components/SignUp";
+import SignUp from "./SignUp";
 import { AuthProvider } from "../context/AuthContext";
-
+import styled from "styled-components";
+const Header = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+	background-color: black;
+	color: white;
+`;
 function App() {
 	return (
 		<BrowserRouter className='App'>
-			<header className='App-header'></header>
 			<AuthProvider>
+				<Header className='App-header'>
+					<h1>Wher's Waldo</h1>
+				</Header>
 				<Switch>
 					<Route exact path='/' component={Home} />
 					<Route exact path='/signup' component={SignUp} />
