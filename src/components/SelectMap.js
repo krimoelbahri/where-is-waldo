34e945bec3a7503d8f0aedd82cfb2dd2bbd8235c
karim <k.guezlane@gmail.com>
2021-08-18 23:02:30 +0100

    created a component to choose a map and difficulty

---
+++ src/components/SelectMap.js
@@ -0,0 +1,77 @@
+import React from "react";
+import styled from "styled-components";
+import Map1 from "../assets/gameImage.jpg";
+import Map2 from "../assets/gameImage2.png";
+import Map3 from "../assets/gameImage3.jpg";
+
+const Container = styled.div`
+	width: 90%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`;
+const FlexR = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: space-around;
+	width: 30%;
+`;
+const MapsContainer = styled(FlexR)`
+	width: 90%;
+	height: 40%;
+`;
+const MapContainer = styled.div`
+	width: 30%;
+	height: 90%;
+`;
+const Image = styled.img`
+	width: 100%;
+	height: 100%;
+`;
+
+export default function SelectMap(props) {
+	function handleImageClick(e) {
+		e.target.style.border = "1px solid red";
+		props.setMapSrc(e.target.currentSrc);
+		props.setMap(e.target.alt);
+	}
+	function handleDifficultyClick(e) {
+		props.setDifficulty(e.target.value);
+	}
+
+	return (
+		<Container>
+			<div>
+				<h1>Choose a Map</h1>
+			</div>
+			<MapsContainer>
+				<MapContainer>
+					<Image onClick={handleImageClick} src={Map1} alt='Map1' />
+				</MapContainer>
+				<MapContainer>
+					<Image onClick={handleImageClick} src={Map2} alt='Map2' />
+				</MapContainer>
+				<MapContainer>
+					<Image onClick={handleImageClick} src={Map3} alt='Map3' />
+				</MapContainer>
+			</MapsContainer>
+			<div>
+				<h1>Difficulty</h1>
+			</div>
+			<FlexR>
+				<button onClick={handleDifficultyClick} value='easy'>
+					Easy
+				</button>
+				<button onClick={handleDifficultyClick} value='medium'>
+					Medium
+				</button>
+				<button onClick={handleDifficultyClick} value='hard'>
+					Hard
+				</button>
+			</FlexR>
+		</Container>
+	);
+}
