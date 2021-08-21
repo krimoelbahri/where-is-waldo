b198cdba8f6847ad0ecaa994a545a8592d295f9a
karim <k.guezlane@gmail.com>
2021-08-21 22:54:34 +0100

    added function to get score data

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,20 +1,47 @@
 import React, { useContext } from "react";
-import { doc, getDoc, setDoc, collection } from "firebase/firestore";
+import {
+	doc,
+	getDoc,
+	getDocs,
+	setDoc,
+	where,
+	orderBy,
+	limit,
+	collection,
+	query,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
 const DataContext = React.createContext();
+
 export function useData() {
 	return useContext(DataContext);
 }
+
 export function DataProvider({ children }) {
-	async function getData(collection, document) {
-		const data = await getDoc(doc(db, collection, document));
+	async function getData(collectionName, document) {
+		const data = await getDoc(doc(db, collectionName, document));
 		return data;
+	}
+	async function getScoreData(collectionName, difficulty) {
+		const q = query(
+			collection(db, collectionName),
+			where("difficulty", "==", difficulty),
+			orderBy("time", "asc"),
+			limit(15)
+		);
+		let scoreArray = [];
+		const querySnapshot = await getDocs(q);
+		querySnapshot.forEach((doc) => {
+			scoreArray.push(doc.data());
+		});
+		return scoreArray;
 	}
 	async function setData(collectionName, data) {
 		await setDoc(doc(collection(db, collectionName)), data);
 	}
-	const value = { getData, setData };
+
+	const value = { getData, setData, getScoreData };
 
 	return (
 		<DataContext.Provider value={value}>
