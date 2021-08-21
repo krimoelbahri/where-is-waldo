874eee3c02456cd139e38a4868d8a5f59c2dc656
karim <k.guezlane@gmail.com>
2021-08-21 20:05:31 +0100

    cleaning reducer

--- src/reducers/timerReducer.js
+++ src/reducers/timerReducer.js
@@ -9,22 +9,6 @@ function timerReducer(state, action) {
 			timeInSeconds: seconds,
 		};
 	}
-
-	//setTimeInSeconds(seconds);
-	//setTime(output);
 }
-let initialState = {
-	start: Date.now(),
-	time: "00:00:00",
-	timeInSeconds: 0,
-};
-export { timerReducer, initialState };
 
-/*
-function timer() {
-    let delta = Date.now() - start; // milliSeconds elapsed since start
-    let output = new Date(delta).toISOString().substr(11, 8); // To HH:MM:SS
-    let seconds = Math.floor(delta / 1000); // converting to seconds
-    setTimeInSeconds(seconds);
-    setTime(output);
-}*/
+export { timerReducer };
