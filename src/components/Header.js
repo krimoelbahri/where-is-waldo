075864712f2600756dfbeb4abb304135800796ee
karim <k.guezlane@gmail.com>
2021-08-22 19:01:28 +0100

    reset data whengoing back to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { useAuth } from "../context/AuthContext";
+import { useMapData } from "../context/MapDataContext";
 
 export default function Header() {
 	const [error, setError] = useState("");
 	const { currentUser, logout } = useAuth();
+	const { setMap, setDifficulty, setLoading } = useMapData();
 
 	async function handleLogout() {
 		try {
@@ -13,11 +15,17 @@ export default function Header() {
 			setError("could not log out");
 		}
 	}
-
+	function handlClick() {
+		setMap("");
+		setDifficulty("");
+		setLoading(true);
+	}
 	return (
 		<>
 			<Link to='/'>
-				<h1>Wher's Waldo</h1>
+				<h1 style={{ color: "white" }} onClick={handlClick}>
+					Wher's Waldo
+				</h1>
 			</Link>
 			<p>{error}</p>
 
