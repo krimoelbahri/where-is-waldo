85eb6d04dc1d79b84379a06979144709b3d7993a
Karim <k.guezlane@gmail.com>
2022-01-25 21:31:05 +0100

    cleaning

--- src/reducers/timerReducer.js
+++ src/reducers/timerReducer.js
@@ -1,7 +1,7 @@
 function timerReducer(state, action) {
 	if (action === "start") {
 		let delta = Date.now() - state.start; // milliSeconds elapsed since start
-		let output = new Date(delta).toISOString().substr(11, 8); // To HH:MM:SS
+		let output = new Date(delta).toISOString(); // To HH:MM:SS
 		let seconds = Math.floor(delta / 1000); // converting to seconds
 		return {
 			...state,
